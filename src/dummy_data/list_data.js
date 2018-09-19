c1fec739d57f99194260fb5df53d495a53c2679b
Scott Bowler <scott.bowler@learningfuze.com>
2018-09-19 15:05:14 -0700

    Completed displaying dummy data

---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, milk, bread, and diet coke',
+        _id: '001'
+    },
+    {
+        title: 'Wash car',
+        details: 'Inside and out',
+        _id: '002'
+    },
+    {
+        title: 'Feed the bear',
+        details: 'She likes steak',
+        _id: '003'
+    },
+    {
+        title: 'Go to gym',
+        details: 'Someday',
+        _id: '004'
+    },
+    {
+        title: 'Learn React',
+        details: 'It\'s a lot of fun',
+        _id: '005'
+    }
+];
