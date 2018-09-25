cab085811e4b524ee2e9d0664612d598d5879419
Scott Bowler <scott.bowler@learningfuze.com>
2018-09-25 14:34:30 -0700

    Completed routing setup

--- src/components/add_item.js
+++ src/components/add_item.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import NavBtn from './nav_btn';
+import config from '../config';
 
 class AddItem extends Component {
     state = {
@@ -6,48 +9,49 @@ class AddItem extends Component {
         details: ''
     }
 
-    handleAddItem = (e) => {
+    handleAddItem = async (e) => {
         e.preventDefault();
+        
+        await axios.post(`${config.API_URL}/todos${config.API_KEY}`, this.state);
 
-        this.props.add(this.state);
-
-        this.setState({
-            title: '',
-            details: ''
-        });
+        this.props.history.push('/');
     }
 
     render(){
         const { title, details } = this.state;
 
         return (
-            <form onSubmit={this.handleAddItem}>
-                <div className="row">
-                    <div className="col s8 offset-s2">
-                        <label>Title</label>
-                        <input 
-                            onChange={(e) => this.setState({title: e.target.value})}
-                            type="text"
-                            value={title}
-                        />
+            <div>
+                <h1 className="center">Add To Do Item</h1>
+                <NavBtn to="/" text="Back To List" color="purple darken-2"/>
+                <form onSubmit={this.handleAddItem}>
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Title</label>
+                            <input
+                                onChange={(e) => this.setState({ title: e.target.value })}
+                                type="text"
+                                value={title}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col s8 offset-s2">
-                        <label>Details</label>
-                        <input
-                            onChange={ ({target}) => this.setState({details: target.value})}
-                            type="text"
-                            value={details}
-                        />
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Details</label>
+                            <input
+                                onChange={({ target }) => this.setState({ details: target.value })}
+                                type="text"
+                                value={details}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col s8 offset-s2 right-align">
-                        <button className="btn purple darken-2">Add Item</button>
+                    <div className="row">
+                        <div className="col s8 offset-s2 right-align">
+                            <button className="btn purple darken-2">Add Item</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         );
     }
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,16 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import NavBtn from './nav_btn';
 import Item from './item';
+import config from '../config';
 
-const List = (props) => {
-    const listElements = props.data.map((item, index) => {
-        return <Item key={item._id} item={item} delete={() => props.delete(item._id)}/>
-    });
+class List extends Component {
+    state = {
+        list: [],
+        error: ''
+    }
 
-    return (
-        <ul className="collection">
-            {listElements}
-        </ul>
-    );
+    componentDidMount() {
+        this.getListData();
+    }
+
+    async getListData() {
+        // Call server to get data
+        // http://api.reactprototypes.com/todos?key=somekey;
+
+        try {
+            const resp = await axios.get(`${config.API_URL}/todos${config.API_KEY}`);
+
+            if (!resp.data.success) {
+                throw new Error('Something went wrong!');
+            }
+
+            this.setState({
+                list: resp.data.todos
+            });
+        } catch (err) {
+
+            this.setState({
+                error: 'Error retrieving list data'
+            });
+        }
+    }
+
+    deleteItem = async id => {
+        await axios.delete(`${config.API_URL}/todos/${id + config.API_KEY}`);
+
+        this.getListData();
+    }
+
+    render() {
+        const { error, list } = this.state;
+
+        const listElements = list.map((item, index) => {
+            return <Item key={item._id} item={item} delete={() => this.deleteItem(item._id)}/>
+        });
+
+        return (
+            <div>
+                <h1 className="center">To Do List</h1>
+                <NavBtn to="/add-item" color="green darken-2" text="Add Item"/>
+                <p className="red-text text-darken-2">{error}</p>
+                <ul className="collection">
+                    {listElements}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default List;
