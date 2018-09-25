44180cc78b7c55847cc2b5a43c167366a82afb6d
Scott Bowler <scott.bowler@learningfuze.com>
2018-09-25 15:46:10 -0700

    Completed details view

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,21 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 import AddItem from './add_item';
 import List from './list';
+import Details from './details';
+import NotFound from './not_found';
 
 class App extends Component {
     render(){
         return (
             <div className="container">
-                <Route path="/" exact component={List}/>
-                <Route path="/add-item" component={AddItem}/>
+                <Switch>
+                    <Route path="/" exact component={List} />
+                    <Route path="/add-item" component={AddItem} />
+                    <Route path="/item/:itemId" component={Details} />
+                    <Route component={NotFound}/>
+                </Switch>
             </div>
         );
     }
